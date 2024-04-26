a7aa84d00827729da51126463dea3994c412fc86
leanor13 <leanor@yandex.ru>
2024-04-26 10:46:30 +0100

    test changes

--- srcs/parsing/main_parsing_yulia.c
+++ srcs/parsing/main_parsing_yulia.c
@@ -6,7 +6,7 @@
 /*   By: yioffe <yioffe@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 12:42:21 by yioffe            #+#    #+#             */
-/*   Updated: 2024/04/25 15:44:06 by yioffe           ###   ########.fr       */
+/*   Updated: 2024/04/26 10:34:48 by yioffe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,31 @@
 
 int	main_parsing(t_shell *shell)
 {
-	char *command = NULL;
+	char	*command = NULL;
 	t_arg	*lst = NULL;
-	t_arg	*lst1;
-	t_arg	*lst1_1;
+	t_arg	*lst1 = NULL;
+	t_arg	*lst1_1 = NULL;
 
-	lst1->append = false;
-	lst1->here_doc = false;
-	lst1->in_file = STDIN_FILENO;
-	lst1->out_file = STDOUT_FILENO;
-	lst1->next = lst1_1;
-	lst1->args = "ls -l";
+	lst1_1 = &(t_arg)
+	{
+		.append = false,
+		.here_doc = false,
+		.in_file = NULL,
+		.out_file = NULL,
+		.next = NULL,
+		.args = "wc"
+	};
+
+	lst1 = &(t_arg)
+	{
+		.append = false,
+		.here_doc = false,
+		.in_file = NULL,
+		.out_file = NULL,
+		.next = lst1_1,
+		.args = "ls -l"
+	};
+
 	//moved argc check to main flow
 	while (1)	
 	{
@@ -42,6 +56,7 @@ int	main_parsing(t_shell *shell)
 		else if (ft_strcmp(command, "pipe 1") == 0)
 			lst = lst1;
 		add_history(command); // do we need to remember if command was executed or not?
+		
 		// I think we need to allocate memory for lst somewhere. Maybe I just did not find it yet.
 		//lst = ft_lexer(command);
 		//lst = ft_parser(lst);
