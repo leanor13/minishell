4ac44b83f1f480dcef764cc60a34db51bcb57b3a
Thuy Valentina Nguyen <thuy-ngu@c1r21s5.42lisboa.com>
2024-05-13 23:28:05 +0100

    updated

--- srcs/parsing/lexer.c
+++ srcs/parsing/lexer.c
@@ -6,7 +6,7 @@
 /*   By: thuy-ngu <thuy-ngu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 12:42:21 by yioffe            #+#    #+#             */
-/*   Updated: 2024/05/13 23:26:23 by thuy-ngu         ###   ########.fr       */
+/*   Updated: 2024/05/13 23:27:58 by thuy-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -160,6 +160,46 @@ int	handle_quotestring(t_sign **lst, char *str, int i)
 		(*lst) = ft_calloc(1, sizeof(t_sign));
 	return(0);
 }
+
+int	find_quote(t_sign **lst, char *str, int i)
+{
+	if ((*lst)->quote_type == DOUBLE_QUOTE)
+	{
+		if (str[i] == 34)
+			return(1);
+		else
+			return(0);
+	}
+	else if ((*lst)->quote_type == SINGLE_QUOTE)
+	{
+		if (str[i] == 39)
+			return(1);
+		else
+			return(0);
+	}
+	else if ((*lst)->quote_type == 0)
+	{
+		if (str[i] == 34)
+		{
+			(*lst)->quote_type = DOUBLE_QUOTE_DIRECT;
+			return(1);
+		}
+		else
+			return(0);
+	}
+	else if ((*lst)->quote_type == 0)
+	{
+		if (str[i] == 39)
+		{
+			(*lst)->quote_type = SINGLE_QUOTE_DIRECT;
+			return(1);
+		}
+		else
+			return(0);
+	}
+	return(0);
+}
+
 
 t_arg	*ft_lexer(char *str, t_arg *lst)
 {
