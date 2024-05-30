a40ba01e0342947f14d910f5b40dba05877eac5c
leanor13 <leanor@yandex.ru>
2024-05-30 17:23:46 +0100

    normi cd

--- srcs/built_ins/ft_cd.c
+++ srcs/built_ins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: yioffe <yioffe@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 15:25:54 by yioffe            #+#    #+#             */
-/*   Updated: 2024/05/30 17:13:49 by yioffe           ###   ########.fr       */
+/*   Updated: 2024/05/30 17:23:02 by yioffe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,11 @@ int	ft_cd(t_shell *shell, t_arg *command)
 	char	*path;
 	char	cwd[PATH_MAX];
 	t_arg	update_cmd;
+	char	*pwd_update;
 
 	if (command->arguments[1] == NULL)
-    {
-        ft_putstr_fd("cd: expected an argument\n", STDERR_FILENO);
-        return (EXIT_FAILURE);
-    }
+		return (ft_putstr_fd("cd: expected an argument\n", STDERR_FILENO),
+			EXIT_FAILURE);
 	else
 		path = command->arguments[1];
 	if (chdir(path) != 0)
@@ -32,18 +31,12 @@ int	ft_cd(t_shell *shell, t_arg *command)
 		return (EXIT_FAILURE);
 	}
 	if (getcwd(cwd, sizeof(cwd)) == NULL)
-	{
-		perror("cd: error retrieving current directory");
-		return (EXIT_FAILURE);
-	}
-	char *pwd_update = ft_strjoin("PWD=", cwd);
+		return (perror("cd: error retrieving current directory"), EXIT_FAILURE);
+	pwd_update = ft_strjoin("PWD=", cwd);
 	if (!pwd_update)
-	{
-		ft_putstr_fd("cd: failed to allocate memory for PWD update\n", STDERR_FILENO);
-		return (EXIT_FAILURE);
-	}
-	update_cmd.arguments = (char*[]){"export", pwd_update, NULL};
+		return (ft_putstr_fd("cd: failed to allocate memory for PWD update\n",
+				STDERR_FILENO), EXIT_FAILURE);
+	update_cmd.arguments = (char *[]){"export", pwd_update, NULL};
 	ft_export(shell, &update_cmd);
-	free(pwd_update);
-	return (EXIT_SUCCESS);
+	return (free(pwd_update), EXIT_SUCCESS);
 }
