056cea02e1f8385161aae664def11e18a1b747e1
leanor13 <leanor@yandex.ru>
2024-05-31 12:02:51 +0100

    add protection

--- srcs/built_ins/ft_cd.c
+++ srcs/built_ins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: yioffe <yioffe@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 15:25:54 by yioffe            #+#    #+#             */
-/*   Updated: 2024/05/30 17:23:02 by yioffe           ###   ########.fr       */
+/*   Updated: 2024/05/31 12:02:43 by yioffe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int	ft_cd(t_shell *shell, t_arg *command)
 	char	cwd[PATH_MAX];
 	t_arg	update_cmd;
 	char	*pwd_update;
+	int		export_status;
 
 	if (command->arguments[1] == NULL)
 		return (ft_putstr_fd("cd: expected an argument\n", STDERR_FILENO),
@@ -27,8 +28,7 @@ int	ft_cd(t_shell *shell, t_arg *command)
 	if (chdir(path) != 0)
 	{
 		ft_putstr_fd("cd: ", STDERR_FILENO);
-		perror(path);
-		return (EXIT_FAILURE);
+		return (perror(path), EXIT_FAILURE);
 	}
 	if (getcwd(cwd, sizeof(cwd)) == NULL)
 		return (perror("cd: error retrieving current directory"), EXIT_FAILURE);
@@ -37,6 +37,6 @@ int	ft_cd(t_shell *shell, t_arg *command)
 		return (ft_putstr_fd("cd: failed to allocate memory for PWD update\n",
 				STDERR_FILENO), EXIT_FAILURE);
 	update_cmd.arguments = (char *[]){"export", pwd_update, NULL};
-	ft_export(shell, &update_cmd);
-	return (free(pwd_update), EXIT_SUCCESS);
+	export_status = ft_export(shell, &update_cmd);
+	return (free(pwd_update), export_status);
 }
