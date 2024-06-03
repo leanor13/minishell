60594b7f3f0661d5f74aaa7c23bcd97c7e075ebd
leanor13 <leanor@yandex.ru>
2024-06-03 11:54:00 +0100

    minor fix with status

--- srcs/main_flow/main_minishell.c
+++ srcs/main_flow/main_minishell.c
@@ -13,14 +13,15 @@
 int	main(int argc, char **argv, char **env)
 {
 	t_shell	shell;
+	int		status;
 
 	(void)argv;
 	if (argc > 1)
 		return (ft_putstr_nl("No arguments allowed for minishell", STDERR_FILENO), EXIT_FAILURE);
 	(void)env;
 	init_shell(&shell, env);
-	main_parsing(&shell);
+	status = main_parsing(&shell);
 	free_shell(&shell);
 	//ft_clean(); - this function will use free_stackfinal as well
-	return (EXIT_FAILURE);
+	return (status);
 }
