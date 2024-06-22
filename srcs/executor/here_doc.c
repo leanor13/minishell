b86b38b58911d302af44980d1a79178ca057dd6a
leanor13 <leanor@yandex.ru>
2024-06-22 13:57:14 +0100

    normi

--- srcs/executor/here_doc.c
+++ srcs/executor/here_doc.c
@@ -6,17 +6,18 @@
 /*   By: yioffe <yioffe@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 00:11:40 by yioffe            #+#    #+#             */
-/*   Updated: 2024/06/21 21:09:17 by yioffe           ###   ########.fr       */
+/*   Updated: 2024/06/22 13:54:45 by yioffe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 #include <signal.h>
 
-t_shell* var()
+t_shell	*var(void)
 {
-	static t_shell tmp;
-	return(&tmp);
+	static t_shell	tmp;
+
+	return (&tmp);
 }
 static char	*get_line(t_shell *shell)
 {
@@ -28,7 +29,7 @@ static char	*get_line(t_shell *shell)
 
 static int	count_limiters(char **limiters)
 {
-	int		lim_count;
+	int	lim_count;
 
 	lim_count = 0;
 	while (limiters[lim_count])
@@ -36,15 +37,13 @@ static int	count_limiters(char **limiters)
 	return (lim_count);
 }
 
-void alex(int code)
+void	alex(int code)
 {
 	if (code == SIGINT)
 	{
-		// printf("alex\n");
 		free_shell(var()->tmp);
 		exit(EXIT_HEREDOC_BREAK);
 	}
-	// return(0);
 }
 
 static void	read_input(int fd[2], char **limiters, t_shell shell)
@@ -60,11 +59,10 @@ static void	read_input(int fd[2], char **limiters, t_shell shell)
 	ft_close(fd[0]);
 	while (true)
 	{
-		// write(shell.std_fds[FD_OUT], "> ", 2);
 		line = get_line(&shell);
-		
-		if (line == NULL || (strncmp(line, limiters[i], strlen(limiters[i])) == 0
-			&& strlen(line) == strlen(limiters[i])))
+		if (line == NULL || (strncmp(line, limiters[i],
+					strlen(limiters[i])) == 0
+				&& strlen(line) == strlen(limiters[i])))
 		{
 			free(line);
 			i++;
@@ -72,10 +70,8 @@ static void	read_input(int fd[2], char **limiters, t_shell shell)
 				break ;
 			continue ;
 		}
-
 		if (i == lim_count - 1)
 			ft_putstr_nl(line, fd[1]);
-		
 		free(line);
 	}
 	ft_close(fd[1]);
@@ -90,8 +86,8 @@ static void	ft_error_forking(void)
 int	here_doc(t_arg *command, t_shell *shell)
 {
 	pid_t	pid;
-	int	fd[2];
-	int	status;
+	int		fd[2];
+	int		status;
 	char	**limiters;
 
 	fd[0] = -1;
