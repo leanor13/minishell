c90d0c76f3ae10158a70445658ce2e3e14e6f180
Yulia Ioffe <yioffe@gmail.com>
2024-06-23 11:27:46 +0100

    fix normi

--- srcs/main_flow/main_util.c
+++ srcs/main_flow/main_util.c
@@ -6,48 +6,13 @@
 /*   By: yioffe <yioffe@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 12:42:21 by yioffe            #+#    #+#             */
-/*   Updated: 2024/06/23 11:22:29 by yioffe           ###   ########.fr       */
+/*   Updated: 2024/06/23 11:26:45 by yioffe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
 volatile int	g_signal;
-
-void	heredoc_handler_function(int sig)
-{
-	int	dev_null;
-
-	if (sig == SIGINT)
-	{
-		dev_null = open("/dev/null", O_RDONLY);
-		if (dev_null != -1)
-		{
-			dup2(dev_null, STDIN_FILENO);
-			close(dev_null);
-		}
-		g_signal = 1;
-		rl_done = 1;
-		rl_on_new_line();
-		rl_replace_line("", 0);
-	}
-}
-
-void	heredoc_signal(void)
-{
-	struct sigaction	signal;
-	struct sigaction	act;
-
-	signal.sa_flags = 0;
-	signal.sa_handler = &heredoc_handler_function;
-	act.sa_flags = 0;
-	act.sa_handler = SIG_IGN;
-	sigemptyset(&signal.sa_mask);
-	sigemptyset(&act.sa_mask);
-	sigaction(SIGINT, &signal, NULL);
-	sigaction(SIGQUIT, &act, NULL);
-	sigaction(SIGTERM, &act, NULL);
-}
 
 void	child_handler_function(int sig)
 {
@@ -66,7 +31,7 @@ void	main_handler_function(int sig)
 	rl_redisplay();
 }
 
-int	dup_stdinp(t_shell *shell)
+int	dup_stds(t_shell *shell)
 {
 	if (dup2(shell->std_fds[0], STDIN_FILENO) == -1)
 	{
@@ -95,31 +60,18 @@ int	main_util(t_shell *shell)
 	exit_status = EXIT_SUCCESS;
 	while (1)
 	{
-		if (dup2(shell->std_fds[0], STDIN_FILENO) == -1)
-		{
-			perror("dup2");
+		if (dup_stds(shell) != EXIT_SUCCESS)
 			return (EXIT_FAILURE);
-		}
-		if (dup2(shell->std_fds[1], STDOUT_FILENO) == -1)
-		{
-			perror("dup2");
-			return (EXIT_FAILURE);
-		}
-		if (dup2(shell->std_fds[2], STDERR_FILENO) == -1)
-		{
-			perror("dup2");
-			return (EXIT_FAILURE);
-		}
 		if (shell->should_exit)
 		{
 			exit_status = shell->exit_status;
 			free_shell(shell);
 			exit(exit_status);
 		}
-		main_signal();//we have to leave this from here*
-		if (g_signal == 1) //WE HAVE TO LEAVE THIS HERE BECAUSE OF THE GLOBAL VARIABLE
+		main_signal();
+		if (g_signal == 1)
 			write(1, "\n", 1);
 		start_minishell(lst, command, shell, exit_status);
 	}
-	return (EXIT_SUCCESS);// *until here
+	return (EXIT_SUCCESS);
 }
